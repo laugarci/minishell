8a9e111a026056615dea1a92ffaa7388eb887c70
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-06-27 11:49:04 +0200

    Started testing get_path, error in environment

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 15:52:21 by laugarci          #+#    #+#             */
-/*   Updated: 2023/06/27 11:30:11 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/06/27 11:47:14 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,24 @@
 #include <readline/readline.h>
 #include <stdlib.h>
 #include "libft.h"
+#include "minishell.h"
 
+static void	free_double(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		free(str[i++]);
+	free(str);
+}
+
+/* Error: Unable to read from envp variable... wtf */
 int	main(int argc, char *argv[], char *envp[])
 {
 	char	*input;
+	char	*aux;
+	char	**tmp;
 
 	if (argc > 1)
 		exit(1);
@@ -26,6 +40,19 @@ int	main(int argc, char *argv[], char *envp[])
 	while (1)
 	{
 		input = readline("minishell > ");
+		if (!ft_strncmp(input, "exit\0", 5))
+		{
+			free(input);
+			exit(0);
+		}
+		tmp = ft_split(input, ' ');
+		aux = get_path(tmp, envp);
+		if (aux)
+		{
+			printf("Path found: %s\n", aux);
+			free(aux);
+		}
+		free_double(tmp);
 		free(input);
 	}
 	return (0);
