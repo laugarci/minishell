feeec3906d35819db2cb6bc5210f3b66c2a69e40
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-07-05 12:56:33 +0200

    Deleted printf and did something that I dont remember

--- src/put_error.c
+++ src/put_error.c
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 13:01:51 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/06/27 13:12:52 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/06/29 13:08:50 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	put_error(char *str, int error_id)
 
 	aux = NULL;
 	if (error_id == 127)
-		aux = ft_strjoin(str, ": command not found\n");
+		aux = ft_strjoin(str, ": No such file or directory\n");
 	else if (error_id == 128)
 		aux = ft_strjoin(str, ": unable to execute command\n");	
 	ft_putstr_fd(aux, 2);
