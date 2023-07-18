4ae0b09893ca7e1b37fd04e29f3ee2ed0cebfd61
Laura Garcia Arch <garch.laura@gmail.com>
2023-07-18 14:06:04 +0200

    pipes working

--- src/exec_pipes.c
+++ src/exec_pipes.c
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 09:52:31 by laugarci          #+#    #+#             */
-/*   Updated: 2023/07/17 17:10:00 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/07/18 14:04:51 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,23 +46,26 @@ int	count_pipes(char *input)
 	return (pipe);
 }
 
-void	exec_pipes(char *input, char **env)
+void exec_pipes(char *input, char **env)
 {
 	int num_pipes;
 	int fds[count_pipes(input)][2];
-	int	status;
+	int status;
 	int i;
 	pid_t pid;
-
+	char	*command;
+	env = NULL; //QUITAR
+	
 	num_pipes = count_pipes(input);
 	i = 0;
-	while(i < num_pipes)
+	while (i < num_pipes)
 	{
 		pipe(fds[i]);
 		i++;
 	}
 	i = 0;
-	while(i <= num_pipes)
+	command = strtok(input, "|");
+	while (command != NULL)
 	{
 		pid = fork();
 		if (pid == -1)
@@ -71,21 +74,38 @@ void	exec_pipes(char *input, char **env)
 		{
 			if (i != 0)
 			{
+				close(fds[i - 1][WRITE_END]);
 				dup2(fds[i - 1][READ_END], STDIN_FILENO);
 				close(fds[i - 1][READ_END]);
-				close(fds[i - 1][WRITE_END]);
 			}
 			if (i != num_pipes)
 			{
-				dup2(fds[i][WRITE_END], STDOUT_FILENO);
 				close(fds[i][READ_END]);
+				dup2(fds[i][WRITE_END], STDOUT_FILENO);
 				close(fds[i][WRITE_END]);
 			}
-			//aqui iria el codigo para ejecutar los comandos
+			char *args[64];
+			int argIndex = 0;
+			char *token = strtok(command, " ");
+			while (token != NULL)
+			{
+				args[argIndex++] = token;
+				token = strtok(NULL, " ");
+			}
+			args[argIndex] = NULL;
+			execvp(args[0], args);
+			exit(0);
 		}
+		else
+		{
+			if (i != 0)
+				close(fds[i - 1][READ_END]);
+			if (i != num_pipes)
+				close(fds[i][WRITE_END]);
+		}
+		command = strtok(NULL, "|");
 		i++;
 	}
-
 	i = 0;
 	while (i < num_pipes)
 	{
