0834eaeda685be204056618c04336610ee35bf4a
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-07-26 17:10:45 +0200

    Added first instance of tokenization in parsing. Saves everything as tokens however has not checked for quotes or token->type..

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 11:42:45 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/07/26 15:40:44 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/07/26 16:14:39 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,9 @@
 
 typedef struct s_token
 {
-	char	*content;
+	char	*string;
 	int		type;
 	int		quotes;
-	void	*next;
 }			t_token;
 
 char		*expand_evals(char *input, char *envp[]);
