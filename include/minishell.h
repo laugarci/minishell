5da441ff2a5a42e345283086202ec6eef7c08084
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-07-26 15:41:15 +0200

    Modified headers to organise better the project

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 11:32:21 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/07/26 15:39:14 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/07/26 15:40:52 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,5 @@ int			count_chars(char *input, char del);
 void		exec_pipes(char *input, char **env, int num_pipes);
 
 char		*ft_strtok(char *str, const char *del);
-
-char		*expand_evals(char *input, char *envp[]);
-int			expansion_amount(char *input);
-char		*find_eval(char *str, char *envp[]);
-
-int			check_quote_state(char *input);
-char		*remove_quotes(char *str);
 
 #endif
--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 11:42:45 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/07/26 15:23:17 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/07/26 15:40:44 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,12 @@ typedef struct s_token
 	int		quotes;
 	void	*next;
 }			t_token;
+
+char		*expand_evals(char *input, char *envp[]);
+int			expansion_amount(char *input);
+char		*find_eval(char *str, char *envp[]);
+
+int			check_quote_state(char *input);
+char		*remove_quotes(char *str);
 
 #endif
