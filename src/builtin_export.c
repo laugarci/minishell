23186bcd4cece05d7b72d63de8242e0d56116198
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-08-31 12:00:12 +0200

    Fixed typos

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 18:22:49 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/08/28 11:58:43 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/08/31 11:58:48 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 #include <stdio.h>
 
-static char	**realloc_envp(char *einval, t_data *data)
+static char	**realloc_envp(char *eval, t_data *data)
 {
 	int		i;
 	char	**og_envp;
@@ -36,7 +36,7 @@ static char	**realloc_envp(char *einval, t_data *data)
 		new_envp[i] = og_envp[i];
 		i++;
 	}
-	new_envp[i] = ft_strdup(einval);
+	new_envp[i] = ft_strdup(eval);
 	if (!new_envp[i++]) // Error: Not enough memory in strdup
 	{
 		free(new_envp);
@@ -46,38 +46,38 @@ static char	**realloc_envp(char *einval, t_data *data)
 	return (new_envp);
 }
 
-static int	modify_einval(char *einval, char *envp[], size_t pos)
+static int	modify_eval(char *eval, char *envp[], size_t pos)
 {
 	if (envp[pos])
 		free(envp[pos]);
 	else
 		return (1);
-	envp[pos] = ft_strdup(einval);
+	envp[pos] = ft_strdup(eval);
 	return (0);
 }
 
-int	builtin_export(char *einval, t_data *data)
+int	builtin_export(char *eval, t_data *data)
 {
 	char	**og_envp;
 	char	**new_envp;
 	int		i;
 	size_t	size;
 
-	if (!ft_strchr(einval, '='))
+	if (!ft_strchr(eval, '='))
 		return (0);
 	i = 0;
 	og_envp = data->envp;
-	size = ft_strlen(einval);
+	size = ft_strlen(eval);
 	while (og_envp[i])
 	{
-		if (!ft_strncmp(einval, og_envp[i], size))
+		if (!ft_strncmp(eval, og_envp[i], size))
 		{
-			modify_einval(einval, og_envp, i);
+			modify_eval(eval, og_envp, i);
 			return (0);
 		}
 		i++;
 	}
-	new_envp = realloc_envp(einval, data);
+	new_envp = realloc_envp(eval, data);
 	if (!new_envp)
 		return (1);
 	free(data->envp);
