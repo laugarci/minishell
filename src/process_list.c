d4813dc37ae4fb93922afd3779a9255a6afcec90
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-09-08 15:42:32 +0200

    added function that returns a list of tokens corresponding to a process

---
+++ src/process_list.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process_list.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 15:24:42 by ffornes-          #+#    #+#             */
+/*   Updated: 2023/09/08 15:40:47 by ffornes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+t_list	*process_list(t_list *lst, int process_id)
+{
+	t_token	*token;
+	t_list	*aux;
+	int		count;
+
+	if (process_id == 0)
+		return (lst);
+	aux = lst;
+	token = aux->content;
+	count = 0;
+	while (count < process_id)
+	{
+		if (token.type ==  PIPE)
+			count++;
+		aux = aux->next;
+		token = aux->content;
+		if (!token->string)
+			return (NULL);
+	}
+	return (aux->next);
+}
