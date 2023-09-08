a430c7c8705498edc1e64ecc8bc28de528781873
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-09-08 19:38:56 +0200

    Fixed rewrite_input errors

--- src/clean_input.c
+++ src/clean_input.c
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 14:53:30 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/09/05 16:18:39 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/09/08 19:38:24 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static char	*allocate_new_redirects(char *str, int i, int *count)
+{
+	if (str[i] == '<' && str[i + 1] != '<' && str[i - 1] != '<')
+	{
+		*count = 1;
+		return (" < ");
+	}
+	else if (str[i] == '<' && (str[i + 1] == '<' || str[i - 1] == '<'))
+	{
+		*count = 2;
+		return (" << ");
+	}
+	else if (str[i] == '>' && str[i + 1] != '>' && str[i - 1] != '>')
+	{
+		*count = 1;
+		return (" > ");
+	}
+	else if (str[i] == '>' && (str[i + 1] == '>' || str[i - 1] == '>'))
+	{
+		*count = 2;
+		return (" >> ");
+	}
+	return (NULL);
+}
+
 static char	*allocate_new_input(char *out, char *str, int i)
 {
 	char	*aux;
+	char	*remains;
+	char	*fix;
+	int		count;
 
 	aux = out;
+	count = 0;
 	if (str[i] == '|')
-		aux = ft_strjoin(aux, " | ");
-	else if (str[i] == '<' && str[i + 1] != '<' && str[i - 1] != '<')
-		aux = ft_strjoin(aux, " < ");
-	else if (str[i] == '<' && (str[i + 1] == '<' || str[i - 1] == '<'))
 	{
-		if (aux[i - 1] == '<')
-			aux[i - 1] = '\0';
-		aux = ft_strjoin(aux, " << ");
+		fix = " | ";
+		count = 1;
 	}
-	else if (str[i] == '>' && str[i + 1] != '>' && str[i - 1] != '>')
-		aux = ft_strjoin(aux, " > ");
-	else if (str[i] == '>' && (str[i + 1] == '>' || str[i - 1] == '>'))
+	else
+		fix = allocate_new_redirects(str, i, &count);
+	if (str[i + count])
 	{
-		if (aux[i - 1] == '>')
-			aux[i - 1] = '\0';
-		aux = ft_strjoin(aux, " >> ");
+		remains = str + i + count;
+		remains = ft_strjoin(fix, remains);
+		aux = ft_strjoin(aux, remains);
+		free(remains);
 	}
+	else
+		return (ft_strjoin(aux, fix));
 	return (aux);
 }
 
@@ -52,15 +79,9 @@ static char	*rewrite_input(char *str, int i)
 		return (NULL);
 	ft_strlcpy(out, str, i + 1);
 	aux = out;
+	printf("OLD INPUT: %s\tSTR: %s\n", out, str);
 	out = allocate_new_input(out, str, i);
-	free(aux);
-	if (!out)
-		return (NULL);
-	aux = out;
-	if (str[i + 1] != '<' && str[i + 1] != '>')
-		out = ft_strjoin(out, (str + i + 1));
-	else
-		out = ft_strjoin(out, (str + i + 2));
+	printf("NEW INPUT: %s\n", out);
 	free(aux);
 	if (!out)
 		return (NULL);
@@ -69,19 +90,17 @@ static char	*rewrite_input(char *str, int i)
 
 static char	*first_pos_re(char *str)
 {
-	if (str[0] == '<' && str[1] != '<' && str[1] != '>'
-		&& str[1] != ' ' && str[1])
+	if (str[0] == '<' && str[1] != '<' && str[1] != ' ' && str[1])
 		return (ft_strjoin("< ", (str + 1)));
-	else if (str[0] == '<' && str[1] == '<' && str[2] != '<' && str[2] != '>'
-		&& str[2] != ' ' && str[2])
+	else if (str[0] == '<' && str[1] == '<' && str[2] != '<'
+			&& str[2] != ' ' && str[2])
 		return (ft_strjoin("<< ", (str + 2)));
-	if (str[0] == '>' && str[1] != '<' && str[1] != '>' && str[1] != ' '
-		&& str[1])
+	if (str[0] == '>' && str[1] != '>' && str[1] != ' ' && str[1])
 		return (ft_strjoin("> ", (str + 1)));
-	else if (str[0] == '>' && str[1] == '>' && str[2] != '<' && str[2] != '>'
+	else if (str[0] == '>' && str[1] == '>' && str[2] != '>'
 		&& str[2] != ' ' && str[2])
 		return (ft_strjoin(">> ", (str + 2)));
-	return (NULL);
+	return (ft_strdup(str));
 }
 
 char	*clean_input(char *str)
@@ -90,11 +109,9 @@ char	*clean_input(char *str)
 	char	*tmp;
 	char	*aux;
 
-	if (str[0] == '<' || str[0] == '>')
-		tmp = first_pos_re(str); // If this is null syntax error near unexpected token 
-	else
-		tmp = ft_strdup(str);
+	tmp = first_pos_re(str); // If this is null syntax error near unexpected token 
 	i = needs_space(tmp);
+	printf("NEEDS SPACE IN POS: %d\n", i);
 	while (i)
 	{
 		if (i < 0) // Error management
@@ -107,7 +124,9 @@ char	*clean_input(char *str)
 				return (NULL); // mem error
 			free(aux);
 		}
+		printf("REWRITTEN INPUT: %s\n", tmp);
 		i = needs_space(tmp);
+		printf("NEEDS SPACE IN POS: %d\n", i);
 	}
 	return (tmp);
 }
