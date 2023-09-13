a73d0d316bd87c256023c8dbf30409369f61e4e7
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-09-13 18:55:11 +0200

    Fixed clean_input

--- src/clean_input.c
+++ src/clean_input.c
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 14:53:30 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/09/08 19:41:11 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/09/13 18:32:35 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,56 +16,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static char	*allocate_new_redirects(char *str, int i, int *count)
-{
-	if (str[i] == '<' && str[i + 1] != '<' && str[i - 1] != '<')
-	{
-		*count = 1;
-		return (" < ");
-	}
-	else if (str[i] == '<' && (str[i + 1] == '<' || str[i - 1] == '<'))
-	{
-		*count = 2;
-		return (" << ");
-	}
-	else if (str[i] == '>' && str[i + 1] != '>' && str[i - 1] != '>')
-	{
-		*count = 1;
-		return (" > ");
-	}
-	else if (str[i] == '>' && (str[i + 1] == '>' || str[i - 1] == '>'))
-	{
-		*count = 2;
-		return (" >> ");
-	}
-	return (NULL);
-}
-
 static char	*allocate_new_input(char *out, char *str, int i)
 {
 	char	*aux;
 	char	*remains;
-	char	*fix;
-	int		count;
 
 	aux = out;
-	count = 0;
-	if (str[i] == '|')
-	{
-		fix = " | ";
-		count = 1;
-	}
-	else
-		fix = allocate_new_redirects(str, i, &count);
-	if (str[i + count])
-	{
-		remains = str + i + count;
-		remains = ft_strjoin(fix, remains);
-		aux = ft_strjoin(aux, remains);
-		free(remains);
-	}
-	else
-		return (ft_strjoin(aux, fix));
+	remains = str + i;
+	remains = ft_strjoin(" ", remains);
+	if (!remains)
+		return (NULL);
+	aux = ft_strjoin(aux, remains);
+	free(remains);
+	if (!aux)
+		return (NULL);
 	return (aux);
 }
 
@@ -107,20 +71,23 @@ char	*clean_input(char *str)
 	char	*tmp;
 	char	*aux;
 
-	tmp = first_pos_re(str); // If this is null syntax error near unexpected token 
+	tmp = first_pos_re(str);
+	if (!tmp)
+	{
+		print_error("Cannot allocate memory\n");
+		return (NULL);
+	}
 	i = needs_space(tmp);
 	while (i)
 	{
-		if (i < 0) // Error management
-			return (NULL);
-		else
+		aux = tmp;
+		tmp = rewrite_input(tmp, i);
+		if (!tmp)
 		{
-			aux = tmp;
-			tmp = rewrite_input(tmp, i);
-			if (!tmp)
-				return (NULL); // mem error
-			free(aux);
+			print_error("Cannot allocate memory\n");
+			return (NULL);
 		}
+		free(aux);
 		i = needs_space(tmp);
 	}
 	return (tmp);
