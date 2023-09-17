26f1b9678c1fa86fcaacb5b3ebfa4a2fbed3d3fc
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-09-17 20:00:48 +0200

    Progress in signals??

--- src/signals.c
+++ src/signals.c
@@ -6,42 +6,79 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 16:24:45 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/09/17 17:47:48 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/09/17 19:56:16 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "minishell.h"
 #include "minishell_defs.h"
 #include <signal.h>
 #include <readline/readline.h>
-
+#include <readline/history.h>
 #include <stdio.h>
 
-static void	state_read(int signal)
+// Using WRITE > PRINTF since PRINTF can crash when used in a signal handler
+
+static void	state_read(int sig, siginfo_t *data, void *n_data)
 {
-	if (signal == SIGINT) // ctrl-C
+	(void) data;
+	(void) n_data;
+	
+	if (sig == SIGINT)
 	{
+		//ft_putstr_fd("Recieved: SIGINT in STATE=READ", 1);
+		ft_putchar_fd('\n', 1);
+		rl_replace_line("", 1);
+		rl_on_new_line();
 		rl_redisplay();
-		printf("\n");
+		ft_putstr_fd("minishell$ ", 1);
+//		ft_putstr_fd("\n", 1);
+//		rl_reset_line_state(); 
 	}
-	else if (signal == SIGQUIT) // ctrl-\ *
-		return ;
+	else if (sig == SIGQUIT)
+		//printf("Recieved: SIGQUIT in STATE=READ\n");
+	return ;
+}
+
+static void	state_hdoc(int sig, siginfo_t *data, void *n_data)
+{
+	(void) data;
+	(void) n_data;
+
+	if (sig == SIGINT)
+		printf("Recieved: SIGINT in STATE=HDOC\n");
+	else if (sig == SIGQUIT)
+		printf("Recieved: SIGQUIT in STATE=HDOC\n");
+	return ;
+}
+
+static void	state_exec(int sig, siginfo_t *data, void *n_data)
+{
+	(void) data;
+	(void) n_data;
+
+	if (sig == SIGINT)
+		printf("Recieved: SIGINT in STATE=EXEC\n");
+	else if (sig == SIGQUIT)
+		printf("Recieved: SIGQUIT in STATE=EXEC\n");
+	return ;
 }
 
 int	*signal_handler(int	*state, int *exit_status)
 {
+	struct sigaction	signal;
+	
+	signal.sa_flags = SA_RESTART;
+	sigemptyset(&signal.sa_mask);
 	if (*state == STATE_READ)
-	{
-		printf("State read\n");
-		signal(SIGINT, (void *)state_read);
-		signal(SIGQUIT, (void *)state_read);
-		signal(SIGTERM, (void  *)state_read);
-	}
+		signal.sa_sigaction = state_read;
 	else if (*state == STATE_HDOC)
-		printf("State here_doc\n");
+		signal.sa_sigaction = state_hdoc;
 	else if (*state == STATE_EXEC)
-		printf("State exec\n");
-
+		signal.sa_sigaction = state_exec;
+	sigaction(SIGINT, &signal, NULL);
+	sigaction(SIGQUIT, &signal, NULL);
 	*exit_status = 0;
 	return (0);
 }
