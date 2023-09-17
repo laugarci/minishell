18dd4e4bcc218b8e1f7e5cb45ac672a7c0a9c85a
Laura Garcia Arch <garch.laura@gmail.com>
2023-09-17 13:36:05 +0200

    h_Doc

--- src/exec_pipes.c
+++ src/exec_pipes.c
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 09:52:31 by laugarci          #+#    #+#             */
-/*   Updated: 2023/09/17 12:17:06 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/09/17 13:35:49 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ int	**pipe_fds(int num_pipes)
 
 void	close_pipes_child(int **fds, int i, int num_pipes, t_list *lst)
 {
-	if (is_type(lst, 2))
+	if ((is_type(lst, 2)))
 		here_doc(lst);
 	if (is_type(lst, 3) || is_type(lst, 4) || is_type(lst, 1))
 	{
@@ -77,14 +77,14 @@ void	close_pipes_child(int **fds, int i, int num_pipes, t_list *lst)
 	if (i != 0)
 	{
 		close(fds[i - 1][WRITE_END]);
-		if (!is_type(lst, 1) && !is_type(lst, 2))
+		if (!is_type(lst, 1))
 			dup2(fds[i - 1][READ_END], STDIN_FILENO);
 		close(fds[i - 1][READ_END]);	
 	}	
 	if (i != num_pipes)
 	{
 		close(fds[i][READ_END]);
-		if (check_redirect(lst) == 0 && !is_type(lst, 2))
+		if (check_redirect(lst) == 0)
 			dup2(fds[i][WRITE_END], STDOUT_FILENO);
 		close(fds[i][WRITE_END]);
 	}
@@ -104,7 +104,9 @@ void	exec_pipes(char **env, int num_pipes, t_list *lst)
 	pid_t	pid;
 	int		**fds;
 	int		check;
+	int		flag;
 
+	flag = 0;
 	check = 0;
 	if (num_pipes)
 		fds = pipe_fds(num_pipes);
