feefde1a5dc36420f47ab73a2efa6a59cc695626
Laura Garcia Arch <laugarci@car7s2.42barcelona.com>
2023-09-20 18:30:48 +0200

    bah

--- src/exec_here_doc.c
+++ src/exec_here_doc.c
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 13:29:17 by laugarci          #+#    #+#             */
-/*   Updated: 2023/09/20 13:08:43 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/09/20 18:28:27 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@
 #include "parser.h"
 #include <readline/readline.h>
 #include <readline/history.h>
+
+#define	READ_END 0
+#define WRITE_END 1
 
 //falta solucionar que si hay una redireccion > se haga tambien
 
@@ -50,39 +53,26 @@ int	find_cat(t_list *aux)
 	return (i);
 }
 
-char	*find_del(t_list *lst, int count)
+char	*find_del(t_list *lst)
 {
 	char	*del;
 	t_token	*token;
-	int		total;
 
+	
 	token = lst->content;
 	if (token->type != 2)
 	{
-		total = count_types(lst, 2) - (count) + 1;
-		token = lst->content;
-		if (token->type != 2)
-		{
-			while (token->type != 2)
-			{
-				lst = lst->next;
-				token = lst->content;
-				if (token->type == 2)
-					total--;
-			}
-		}
-		while (total > 0)
+		while (token->type != 2)
 		{
 			lst = lst->next;
 			token = lst->content;
-			if (token->type == 2)
-				total--;
 		}
 	}
 	del = malloc(sizeof(char) * ft_strlen(token->string) + 1);
 	if (!del)
 		return (NULL);
 	ft_strlcpy(del, token->string, ft_strlen(token->string) + 1);
+	printf("del >> %s\n", del);
 	return (del);
 }
 
@@ -98,7 +88,7 @@ void	here_doc_cmd(t_list *lst, char *text)
 		if ((is_type(lst, 3) || is_type(lst, 4)))
 			exec_redirect(lst, 1);
 		write(1, text, ft_strlen(text));
-		if (!is_type(lst, PIPE))
+		if (is_type(lst, PIPE))
 			exit(1);
 	}
 }
@@ -108,28 +98,25 @@ int	here_doc(t_list *lst)
 	char	*del;
 	char	*input;
 	char	*text;
-	int		count;
+	t_token	*token;
 
-	count = count_types(lst, 2);
-	while (count > 0)
-	{
-		text = NULL;
-		del = find_del(lst, count);
-		printf("del >> %s\n", del);
-		while(42)
-		{	
-			input = readline("> ");
-			if (ft_strncmp(input, del, ft_strlen(del) + 1) == 0)
-				break ;
-			text = ft_strjoin(text, input);
-			text = ft_strjoin(text, "\n");
-//		input = expand_evals(input, env);
-		}
-		if (is_type(lst, PIPE))
-			lst = move_to_pipe(lst);
-		count--;
+	token = lst->content;
+	text = NULL;
+	del = find_del(lst);
+	while(42)
+	{	
+		input = readline("> ");
+		if (ft_strncmp(input, del, ft_strlen(del) + 1) == 0)
+			break ;
+		text = ft_strjoin(text, input);
+		text = ft_strjoin(text, "\n");
 	}
-	if (count_list(lst) > 2)
-		here_doc_cmd(lst, text);
+	if (ft_strncmp(token->string, "cat\0", 4) == 0)
+	{
+		if (is_type(lst, 3) || is_type(lst, 4))
+			exec_redirect(lst, 1);
+		write(1, text, ft_strlen(text));
+	}
+//	here_doc_cmd(lst, text);
 	return (0);
 }
