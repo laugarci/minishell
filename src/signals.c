c7db8643f25197cd1716c3b5d958ea30bc016823
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-09-24 17:47:56 +0200

    Did I fix something here?

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 16:24:45 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/09/24 17:29:22 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/09/24 17:32:39 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,6 @@
 #include <readline/history.h>
 #include <stdlib.h>
 #include "termios.h"
-
-void	signal_display(int mode)
-{
-	struct termios	tc;
-
-	tcgetattr(0, &tc);
-	tc.c_lflag &= ~ECHOCTL;
-	if (mode)
-		tc.c_lflag |= ECHOCTL;
-	tcsetattr(0, TCSANOW, &tc);
-}
 
 int	set_or_return_state(int mode, int value)
 {
