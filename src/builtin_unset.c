9fb0d77d4f94da91d8d8ad6b1aec7eb210308aa8
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2023-09-25 19:34:16 +0200

    Fixed unset

--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -6,16 +6,46 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 17:48:17 by ffornes-          #+#    #+#             */
-/*   Updated: 2023/09/22 18:59:37 by ffornes-         ###   ########.fr       */
+/*   Updated: 2023/09/25 19:33:16 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "libft.h"
-#include "minishell.h"
 #include "libft_bonus.h"
+#include "minishell.h"
 #include "parser.h"
+
+static int	invalid_identifier(char *str)
+{
+	ft_putstr_fd("minishell: unset: `", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd("\': not a valid identifier\n", 2);
+	return (1);
+}
+
+static int	is_valid_input(char *str, int i)
+{
+	int	flag;
+
+	flag = 0;
+	if (!str[i])
+		return (invalid_identifier(""));
+	while (str[i])
+	{
+		if (!i && !ft_isalpha(str[i]) && str[i] != '_')
+			flag++;
+		else if (!ft_isalnum(str[i]) && str[i] != '_')
+		{
+			if (str[i] == '+' && str[i + 1] == '=')
+				return (1);
+			flag++;
+		}
+		if (flag)
+			return (invalid_identifier(str));
+		i++;
+	}
+	return (1);
+}
 
 static void	builtin_unset_util(t_list **previous, t_list *var_lst, \
 			t_list **env_lst)
@@ -33,24 +63,18 @@ static void	builtin_unset_util(t_list **previous, t_list *var_lst, \
 	ft_lstclear(&var_lst, (void *)free_var);
 }
 
-int	builtin_unset(t_list *lst, t_list **env_lst)
+static int	unset_loop(t_token *token, t_list **env_lst)
 {
-	t_token	*token;
-	t_list	*aux;
 	t_list	*previous;
 	t_list	*var_lst;
 	t_env	*var;
 
-	aux = lst->next;
-	token = aux->content;
-	if (!lst->next || !aux->content || !token->string)
-		return (0);
 	var_lst = *env_lst;
 	previous = NULL;
 	while (var_lst)
 	{
 		var = var_lst->content;
-		if (!ft_strncmp(var->key, token->string, ft_strlen(token->string)))
+		if (!ft_strncmp(var->key, token->string, ft_strlen(token->string) + 1))
 		{
 			builtin_unset_util(&previous, var_lst, env_lst);
 			break ;
@@ -59,4 +83,31 @@ int	builtin_unset(t_list *lst, t_list **env_lst)
 		var_lst = var_lst->next;
 	}
 	return (0);
+}
+
+int	builtin_unset(t_list *lst, t_list **env_lst)
+{
+	t_token	*token;
+	t_list	*var_lst;
+	int		err;
+
+	lst = lst->next;
+	token = lst->content;
+	if ((!token->string || !*token->string) && token->quotes < 0)
+		return (0);
+	err = 0;
+	var_lst = *env_lst;
+	while (lst->next)
+	{
+		if (!is_valid_input(token->string, 0))
+			err = 1;
+		unset_loop(token, env_lst);
+		lst = lst->next;
+		token = lst->content;
+		if ((!token->string || !*token->string) && token->quotes < 0)
+			break ;
+		else if (token && token->type >= 0)
+			break ;
+	}
+	return (err);
 }
